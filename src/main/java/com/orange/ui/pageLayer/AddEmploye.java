8c921f5a7aeea958015053d2ec266a0be70cf369
pradnya <pradnya@192.168.1.5>
2025-07-03 13:48:27 +0530

    AddEmploye created by By Pradnya

---
+++ src/main/java/com/orange/ui/pageLayer/AddEmploye.java
@@ -0,0 +1,13 @@
+package com.orange.ui.pageLayer;
+
+public class AddEmploye {
+	
+	public  void validateaddemployeefunctinality(String firstname,String lastname,String city)
+	{
+		
+		System.out.println(firstname);
+		System.out.println(lastname);
+		System.out.println(city);
+	}
+
+}
---
+++ src/test/java/com/orange/ui/testLayer/AddEmployeTest.java
@@ -0,0 +1,14 @@
+package com.orange.ui.testLayer;
+
+import com.orange.ui.pageLayer.AddEmploye;
+
+public class AddEmployeTest {
+	
+	public static void main(String args[])
+	{
+		AddEmploye emp= new AddEmploye();
+		
+		emp.validateaddemployeefunctinality("sonali", "Bhosale", "kolhapur");
+	}
+
+}
