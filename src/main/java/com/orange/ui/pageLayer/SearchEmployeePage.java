001a523ed8e1af511930d2d0522f2263b5b5cfe3
ADMIN NP <ADMIN NP@DESKTOP-7NRAK7B.alphionsee.in>
2025-07-03 16:35:37 +0530

    SearchEmployeePage test cases by Neha

---
+++ src/main/java/com/orange/ui/pageLayer/SearchEmployeePage.java
@@ -0,0 +1,10 @@
+package com.orange.ui.pageLayer;
+
+public class SearchEmployeePage {
+
+	public void validateSearchEmployeePage(String eName, int eID)
+	{
+		System.out.println(eName);
+		System.out.println(eID);
+	}
+}
--- src/test/java/com/orange/ui/testLayer/LoginPageTest.java
+++ src/test/java/com/orange/ui/testLayer/LoginPageTest.java
@@ -5,6 +5,7 @@ import com.orange.ui.pageLayer.AddUser;
 import com.orange.ui.pageLayer.ContactPage;
 import com.orange.ui.pageLayer.HomePage;
 import com.orange.ui.pageLayer.LoginPage;
+import com.orange.ui.pageLayer.SearchEmployeePage;
 import com.orange.ui.pageLayer.SignUpPage;
 
 public class LoginPageTest {
@@ -15,25 +16,39 @@ public class LoginPageTest {
 		login.valiadateLoginFuncationality("Pratik@gamil.com", "Pratik@123");
 		
 		
+		
 		SignUpPage sign =new SignUpPage();
 		
 		sign.validateSignUPFunctionality("Hemant", "Kamble", "Male");
 		
+		
+		
 		HomePage homePage=new HomePage();
 		homePage.validateHomePageFunctionality("OranageHrm");
+		
+		
 		
 		ContactPage page = new ContactPage();
 		
 		page.validateContactPageFunctionality("Prajakta", "Jamdar");
 		
 		
+		
 		AddPersonalDetailsPage personalDetails = new AddPersonalDetailsPage();
 		
 		personalDetails.validateAddPersonalDetailsPage("Komal", "Dhole", "Female");
 		
+		
+		
 		AddUser obj = new AddUser();
 		
 		obj.addUserFunctionality("Aditya", "Pawar");
+		
+		
+		
+		SearchEmployeePage searchEmp = new SearchEmployeePage();
+		
+		searchEmp.validateSearchEmployeePage("Neha", 29);
 		
 	}
 
